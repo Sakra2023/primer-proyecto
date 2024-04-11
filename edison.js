b1a936bf9d70a77a92b31bfcf672bfed61eaf5c3
Edison Vargas <165614274+Sakra2023@users.noreply.github.com>
2024-04-11 17:24:01 -0500

    creamos es archivo  edison.js en github

---
+++ edison.js
@@ -0,0 +1,4 @@
+let nombre=prompt("INGRESA TU NOMBRE:")
+let edad =parseInt(prompt("INGRESA TU EDAD:"))
+document.write("HOLA TU NOMBRE ES: "+nombre+"<br>")
+document.write("HOLA TU EDAD ES: "+edad+"<br>")
